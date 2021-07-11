cef10108225061e73baab75e4873bd4a721b66fc
Xavier Martinez <blackshieldsx@gmail.com>
2021-07-10 18:28:36 -0700

    Setting up the core

--- env/Scripts/django-admin.py
+++ env/Scripts/django-admin.py
@@ -1,4 +1,4 @@
-#!c:\users\cyran\dev\podsquadz\ps-ambassador\env\scripts\python.exe
+#!C:\Users\cyran\dev\podsquadz\ps-ambassador\env\Scripts\python.exe
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
