093926858ba1c377b0394ec476f7b6013f41e5f7
Xavier Martinez <blackshieldsx@gmail.com>
2021-07-23 17:31:43 -0700

    Working on the waiting db

--- core/management/commands/wait_for_db.py
+++ core/management/commands/wait_for_db.py
@@ -1,1 +1,17 @@
-from django.core
+from django.core.management import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+import time
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write('Waiting for the database...')
+        conn = None
+
+        while not conn:
+            try:
+                conn = connections['default']
+            except OperationalError:
+        self.stdout.write('Waiting for the database...')
+
+
